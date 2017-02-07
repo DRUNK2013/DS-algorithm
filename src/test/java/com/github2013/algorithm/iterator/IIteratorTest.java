32ca2f0d6ff72dc5d90efd40272d2e075b0b9bea
370534549@qq.com <370534549@qq.com>
2017-02-07 23:58:12 +0800

    add sequence test suite

---
+++ src/test/java/com/github2013/algorithm/iterator/IIteratorTest.java
@@ -0,0 +1,29 @@
+package com.github2013.algorithm.iterator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by drunk on 17-2-7.
+ * Author : DRUNK
+ * email :len1988.zhang@gmail.com
+ */
+public class IIteratorTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void hasNext() throws Exception {
+
+    }
+
+    @Test
+    public void getNext() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/com/github2013/algorithm/sequence/ISequenceTest.java
@@ -0,0 +1,29 @@
+package com.github2013.algorithm.sequence;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by drunk on 17-2-7.
+ * Author : DRUNK
+ * email :len1988.zhang@gmail.com
+ */
+public class ISequenceTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void rank2Pos() throws Exception {
+
+    }
+
+    @Test
+    public void pos2Rank() throws Exception {
+
+    }
+
+}
