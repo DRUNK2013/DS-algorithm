a2d49cd840a204139db7bcd59e425cf6ac51afbd
370534549@qq.com <370534549@qq.com>
2017-02-11 21:56:57 +0800

    binary tree

--- src/main/java/com/github2013/algorithm/tree/IBinTree.java
+++ src/main/java/com/github2013/algorithm/tree/IBinTree.java
@@ -6,9 +6,59 @@ package com.github2013.algorithm.tree;
  * email :len1988.zhang@gmail.com
  */
 
+import com.github2013.algorithm.iterator.IIterator;
+
 /**
  * 二叉树接口
  */
 public interface IBinTree {
-    //返回树根
+
+    /**
+     * 返回根节点
+     * @return
+     */
+    BinTreePosition getRoot();
+
+    /**
+     * 判断是否树空
+     * @return
+     */
+    boolean isEmpty();
+
+    /**
+     * 返回树的规模(即树根的后代数目)
+     * @return
+     */
+    int getSize();
+
+    /**
+     * 返回树(根)的高度
+     * @return
+     */
+    int getHeight();
+
+    /**
+     * 前序遍历
+     * @return
+     */
+    IIterator elementsPreorder();
+
+    /**
+     * 中序遍历
+     * @return
+     */
+    IIterator elementsInorder();
+
+    /**
+     * 后续遍历
+     * @return
+     */
+    IIterator elementsPostorder();
+
+    /**
+     * 层次遍历
+     * @return
+     */
+    IIterator elementsLevelorder();
+
 }
---
+++ src/main/java/com/github2013/algorithm/tree/impl/BinTreeNodeImpl.java
@@ -0,0 +1,84 @@
+package com.github2013.algorithm.tree.impl;
+
+import com.github2013.algorithm.iterator.IIterator;
+import com.github2013.algorithm.tree.BinTreePosition;
+import com.github2013.algorithm.tree.IBinTree;
+
+/**
+ * Created by drunk on 17-2-11.
+ * Author : DRUNK
+ * email :len1988.zhang@gmail.com
+ */
+public class BinTreeNodeImpl implements IBinTree {
+    /**
+     * 返回根节点
+     *
+     * @return
+     */
+    public BinTreePosition getRoot() {
+        return null;
+    }
+
+    /**
+     * 判断是否树空
+     *
+     * @return
+     */
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * 返回树的规模(即树根的后代数目)
+     *
+     * @return
+     */
+    public int getSize() {
+        return 0;
+    }
+
+    /**
+     * 返回树(根)的高度
+     *
+     * @return
+     */
+    public int getHeight() {
+        return 0;
+    }
+
+    /**
+     * 前序遍历
+     *
+     * @return
+     */
+    public IIterator elementsPreorder() {
+        return null;
+    }
+
+    /**
+     * 中序遍历
+     *
+     * @return
+     */
+    public IIterator elementsInorder() {
+        return null;
+    }
+
+    /**
+     * 后续遍历
+     *
+     * @return
+     */
+    public IIterator elementsPostorder() {
+        return null;
+    }
+
+    /**
+     * 层次遍历
+     *
+     * @return
+     */
+    public IIterator elementsLevelorder() {
+        return null;
+    }
+}
