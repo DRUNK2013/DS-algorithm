e8efbe9ece3f32c72e01f5d8d6ccf3a155870d3e
370534549@qq.com <370534549@qq.com>
2017-03-12 23:56:04 +0800

    AVLTree

--- src/main/java/com/github2013/algorithm/tree/AVLTree.java
+++ src/main/java/com/github2013/algorithm/tree/AVLTree.java
@@ -5,8 +5,100 @@ package com.github2013.algorithm.tree;
  */
 
 /**
+ * 平衡二叉树,
+ * 定义:平衡二叉树是一个特殊的二叉排序树,其左子树和右子树都是平衡二叉树.
+ * 且左子树和右子树的深度之差,不超过1
+ * 平衡因子:可以定义为左子树的深度减去右子树的深度.
+ * 平衡二叉树是对二叉树的优化,二叉树在极端的情况下,是一个单链表.
+ * 平衡二叉树查找元素的次数不超过树的深度,时间复杂度为:logN
  *
  * @param <E>
  */
 public class AVLTree<E> {
+    //定义左右子树之间深度差,1:左侧高,0:等高,-1:右侧高
+    enum Balance {
+        LH(1), EH(0), RH(-1);
+        private final int value;
+
+        Balance(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    //定义平衡二叉树的节点信息
+    private class TreeNode<E> {
+        private E element;
+        private TreeNode<E> parent;
+        private TreeNode<E> left;
+        private TreeNode<E> right;
+        private int balance = Balance.EH.getValue();
+
+        public TreeNode(E element, TreeNode<E> parent) {
+            this.element = element;
+            this.parent = parent;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public void setElement(E element) {
+            this.element = element;
+        }
+
+        public TreeNode<E> getParent() {
+            return parent;
+        }
+
+        public void setParent(TreeNode<E> parent) {
+            this.parent = parent;
+        }
+
+        public TreeNode<E> getLeft() {
+            return left;
+        }
+
+        public void setLeft(TreeNode<E> left) {
+            this.left = left;
+        }
+
+        public TreeNode<E> getRight() {
+            return right;
+        }
+
+        public void setRight(TreeNode<E> right) {
+            this.right = right;
+        }
+
+        public int getBalance() {
+            return balance;
+        }
+
+        public void setBalance(int balance) {
+            this.balance = balance;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s,Balance=%d", element.toString(), balance);
+        }
+    }
+
+    //根节点
+    private TreeNode<E> root = null;
+
+    //树中元素个数
+    private int size;
+
+    public AVLTree() {
+        this.size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
 }
--- src/main/java/com/github2013/algorithm/tree/BinaryTree_v2.java
+++ src/main/java/com/github2013/algorithm/tree/BinaryTree_v2.java
@@ -4,75 +4,75 @@ import sun.reflect.generics.tree.Tree;
 
 //http://www.cnblogs.com/lzq198754/p/5857597.html
 //定义数据结构节点
-class TreeNode {
-    //左节点
-    private TreeNode leftNode;
-
-    //右节点
-    private TreeNode rightNode;
-
-    //数据节点
-    private Object value;
-
-    private boolean isDelete;
-
-    public TreeNode getLeftNode() {
-        return this.leftNode;
-    }
-
-    public void setLeftNode(TreeNode leftNode) {
-        this.leftNode = leftNode;
-    }
-
-    public TreeNode getRightNode() {
-        return this.rightNode;
-    }
-
-    public void setRihtNode(TreeNode rightNode) {
-        this.rightNode = rightNode;
-    }
-
-    public Object getValue() {
-        return this.value;
-    }
-
-    public void setValue(Object value) {
-        this.value = value;
-    }
-
-    public boolean GetIsDelete() {
-        return this.isDelete;
-    }
-
-    public void setIsDelete(boolean isDelete) {
-        this.isDelete = isDelete;
-    }
-
-    public TreeNode() {
-        super();
-    }
-
-    public TreeNode(Object value) {
-        this(null, null, value, false);
-    }
-
-    public TreeNode(TreeNode leftNode, TreeNode rightNode, Object value, boolean isDelete) {
-        super();
-        this.leftNode = leftNode;
-        this.rightNode = rightNode;
-        this.value = value;
-        this.isDelete = isDelete;
-    }
-
-    @Override
-    public String toString() {
-        return "lleftNode:" + this.leftNode +
-                "\nrightNode:" + this.rightNode +
-                "\nvalue:" + this.value;
-    }
-}
-
 public class BinaryTree_v2 {
+
+    class TreeNode {
+        //左节点
+        private TreeNode leftNode;
+
+        //右节点
+        private TreeNode rightNode;
+
+        //数据节点
+        private Object value;
+
+        private boolean isDelete;
+
+        public TreeNode getLeftNode() {
+            return this.leftNode;
+        }
+
+        public void setLeftNode(TreeNode leftNode) {
+            this.leftNode = leftNode;
+        }
+
+        public TreeNode getRightNode() {
+            return this.rightNode;
+        }
+
+        public void setRihtNode(TreeNode rightNode) {
+            this.rightNode = rightNode;
+        }
+
+        public Object getValue() {
+            return this.value;
+        }
+
+        public void setValue(Object value) {
+            this.value = value;
+        }
+
+        public boolean GetIsDelete() {
+            return this.isDelete;
+        }
+
+        public void setIsDelete(boolean isDelete) {
+            this.isDelete = isDelete;
+        }
+
+        public TreeNode() {
+            super();
+        }
+
+        public TreeNode(Object value) {
+            this(null, null, value, false);
+        }
+
+        public TreeNode(TreeNode leftNode, TreeNode rightNode, Object value, boolean isDelete) {
+            super();
+            this.leftNode = leftNode;
+            this.rightNode = rightNode;
+            this.value = value;
+            this.isDelete = isDelete;
+        }
+
+        @Override
+        public String toString() {
+            return "lleftNode:" + this.leftNode +
+                    "\nrightNode:" + this.rightNode +
+                    "\nvalue:" + this.value;
+        }
+    }
 
     //根节点
     private TreeNode root;
